b885968ffcc0bd178c85eb5c3e1542b9abf1795e
Christine Nuq <cnuqui@icloud.com>
2023-04-17 21:47:39 -0400

    fix auth

--- src/router/person.js
+++ src/router/person.js
@@ -3,9 +3,9 @@ const PersonController = require("../controller/person");
 const isAuthenticated = require('../middleware/isAuthenticated');
 
 personRouter = Router();
+personRouter.use(isAuthenticated);
 personRouter.get("/", PersonController.getAll);
 personRouter.get("/:id", PersonController.getOne);
-//personRouter.use(isAuthenticated)
 personRouter.post("/", PersonController.create);
 personRouter.put("/:id", PersonController.replace);
 personRouter.patch("/:id", PersonController.update);
