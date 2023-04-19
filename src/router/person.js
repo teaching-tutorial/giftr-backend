4879cd94dbb82309163e64baf604ecba3048b5e1
Christine Nuq <cnuqui@icloud.com>
2023-04-19 19:13:59 -0400

    updates oauth

--- src/router/person.js
+++ src/router/person.js
@@ -12,9 +12,9 @@ personRouter.put("/:id", PersonController.replace);
 personRouter.patch("/:id", PersonController.update);
 personRouter.delete("/:id", PersonController.deleteOne);
 
-personRouter.get("/:id/gifts", GiftController.getAll);
-personRouter.get("/:id/gifts/:gid", GiftController.getById);
-personRouter.post("/:id/gifts", GiftController.create);
-personRouter.patch("/:id/gifts/:gid", GiftController.update);
-personRouter.delete("/:id/gifts/:gid", GiftController.deleteOne);
+// personRouter.get("/:id/gifts", GiftController.getAll);
+// personRouter.get("/:id/gifts/:gid", GiftController.getById);
+// personRouter.post("/:id/gifts", GiftController.create);
+// personRouter.patch("/:id/gifts/:gid", GiftController.update);
+// personRouter.delete("/:id/gifts/:gid", GiftController.deleteOne);
 module.exports = personRouter;
